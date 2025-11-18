f941007a0e05bd6558a024fee30c3c4525fbd50b
Verisana <account@0112.pro>
2025-11-18 18:48:03 +0000

    feat: make vm runtime accessible

--- sui-execution/src/latest.rs
+++ sui-execution/src/latest.rs
@@ -38,7 +38,7 @@ use crate::executor;
 use crate::verifier;
 use sui_adapter_latest::execution_mode;
 
-pub(crate) struct Executor(Arc<MoveVM>);
+pub struct Executor(pub Arc<MoveVM>);
 
 pub(crate) struct Verifier<'m> {
     config: VerifierConfig,
